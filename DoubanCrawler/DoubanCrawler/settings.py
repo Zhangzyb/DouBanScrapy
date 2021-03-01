015813bafeacc49013d09610484488514e0c6607
zybsw <841733577@qq.com>
2021-03-01 15:11:09 +0800

    相关配置

--- DoubanCrawler/DoubanCrawler/settings.py
+++ DoubanCrawler/DoubanCrawler/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'DoubanCrawler.spiders'
 #USER_AGENT = 'DoubanCrawler (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -37,34 +37,36 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'DoubanCrawler.middlewares.DoubancrawlerSpiderMiddleware': 543,
-#}
+# SPIDER_MIDDLEWARES = {
+#     'DoubanCrawler.middlewares.DoubancrawlerSpiderMiddleware': 543,
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'DoubanCrawler.middlewares.DoubancrawlerDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'DoubanCrawler.middlewares.RandomUserAgentMiddleware': 1,
+    'DoubanCrawler.middlewares.DoubancrawlerDownloaderMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'DoubanCrawler.pipelines.DoubancrawlerPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'DoubanCrawler.pipelines.MysqlPipeline':300,
+#    'DoubanCrawler.pipelines.DoubancrawlerPipeline': 300
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -86,3 +88,10 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+MYSQL_HOST = 'localhost'
+MYSQL_DATABASE = 'api'
+MYSQL_PORT = 3306
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = '954590394@qq.com'
+RETRY_HTTP_CODES = [302, 401, 403, 408, 414, 500, 502, 503, 504]
