142633f27ce5017a603d4e3488c480a227fa3534
zybsw <841733577@qq.com>
2021-03-01 15:08:52 +0800

    定义电影及评论字段

--- DoubanCrawler/DoubanCrawler/items.py
+++ DoubanCrawler/DoubanCrawler/items.py
@@ -1,12 +1,27 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
 class DoubancrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    table = 'movie'
+    movieId = scrapy.Field()
+    title = scrapy.Field()
+    year = scrapy.Field()
+    directors = scrapy.Field()
+    casts = scrapy.Field()
+    region = scrapy.Field()
+    types = scrapy.Field()
+    duration = scrapy.Field()
+    rate = scrapy.Field()
+    cover = scrapy.Field()
+    star = scrapy.Field()
+    rating_num = scrapy.Field()
+
+
+class DoubanCommentItem(scrapy.Item):
+    table = 'movie_comment'
+    movie = scrapy.Field()
+    username = scrapy.Field()
+    avatar = scrapy.Field()
+    rate = scrapy.Field()
+    time = scrapy.Field()
+    content = scrapy.Field()
+
